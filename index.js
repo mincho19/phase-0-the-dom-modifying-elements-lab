eda80a267d63969e8d657fb23bf2149dce520cd9
Min Cho <minsangcho19@gmail.com>
2021-12-05 01:04:48 -0800

    i am the champion

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code here!
+
+/*
+const element = document.createElement('div');
+document.body.append(element);
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+
+*/
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', "victory");
+newHeader.textContent = "Min is the champion";
+document.body.append(newHeader);
+
+
+
+
+
+//const newHeader = document.createElement("YOUR-NAME is the champion");
+
+
+
